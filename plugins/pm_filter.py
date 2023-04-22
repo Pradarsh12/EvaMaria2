45cafaadf2791dfb37fbeea2d26e19c93ba54bee
Pradarsh12 <117892231+Pradarsh12@users.noreply.github.com>
2023-04-22 23:09:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1262,7 +1262,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("👩‍🦯 𝖡𝖺𝖼𝗄", callback_data="start"),
-                    InlineKeyboardButton("📞 𝖢𝗈𝗇𝗍𝖺𝖼𝗍", url="t.me/Search_Zone_Support")
+                    InlineKeyboardButton("📞 𝖢𝗈𝗇𝗍𝖺𝖼𝗍", url="t.me/soonbotmaker0")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
